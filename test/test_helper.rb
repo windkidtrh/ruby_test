70a6eb8e3ee19446d0c7332f46d34db616cc5340
Bo <1442983474@qq.com>
2018-04-02 16:16:39 +0800

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,4 +14,18 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
+  # 登入指定的用户
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
+end
+
+class ActionDispatch::IntegrationTest
+  # 登入指定的用户
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
